e3e8473251376be065653bd6a112191be94ebbd3
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-08 16:17:11 +0200

    Possibility to add and view administrator added

---
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/ViewModels/AdministratorEditFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Models;
+using Zadatak_1.Views;
+
+namespace Zadatak_1.ViewModels
+{
+    class AdministratorEditFormViewModel : BaseViewModel
+    {
+        AdministratorEditFormView administratorFormView;        
+
+        private vwAdministrator administrator;
+
+        public vwAdministrator Administrator
+        {
+            get
+            {
+                return administrator;
+            }
+            set
+            {
+                administrator = value;
+                OnPropertyChanged("Administrator");
+            }
+        }
+        public AdministratorEditFormViewModel(AdministratorEditFormView administratorFormView, vwAdministrator administratorToEdit)
+        {
+            this.administratorFormView = administratorFormView;
+            Administrator = administratorToEdit;            
+        }
+    }
+}
---
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/Views/AdministratorEditFormView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Zadatak_1.Models;
+using Zadatak_1.ViewModels;
+
+namespace Zadatak_1.Views
+{
+    /// <summary>
+    /// Interaction logic for AdministratorEditFormView.xaml
+    /// </summary>
+    public partial class AdministratorEditFormView : Window
+    {
+        public AdministratorEditFormView(vwAdministrator administratorToEdit)
+        {
+            InitializeComponent();
+            this.DataContext = new AdministratorEditFormViewModel(this, administratorToEdit);
+        }
+    }
+}
