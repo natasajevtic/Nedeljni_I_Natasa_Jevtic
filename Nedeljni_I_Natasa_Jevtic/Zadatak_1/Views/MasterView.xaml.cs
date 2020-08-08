fb95ddae69d7eac7deab025afcfae878d953937b
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-08 15:46:23 +0200

    User interface for master created

--- Nedeljni_I_Natasa_Jevtic/Zadatak_1/Views/MasterView.xaml.cs
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/Views/MasterView.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Zadatak_1.ViewModels;
 
 namespace Zadatak_1.Views
 {
@@ -21,7 +13,52 @@ namespace Zadatak_1.Views
     {
         public MasterView()
         {
-            InitializeComponent();
+            InitializeComponent();            
+            this.DataContext = new MasterViewModel();
+
+            var menuAdministrator = new List<SubItem>();
+            menuAdministrator.Add(new SubItem("View all administrators", new AdministratorsView()));
+            var item1 = new ItemMenu("Administrators", menuAdministrator, PackIconKind.PersonBoxOutline);
+
+            var menuManager = new List<SubItem>();
+            menuManager.Add(new SubItem("View all managers"));
+            var item2 = new ItemMenu("Managers", menuManager, PackIconKind.PersonBoxOutline);
+
+            var menuEmployee = new List<SubItem>();
+            menuEmployee.Add(new SubItem("View all employees"));
+            var item3 = new ItemMenu("Employees", menuEmployee, PackIconKind.PersonBoxOutline);
+
+            var menuProject = new List<SubItem>();
+            menuProject.Add(new SubItem("View all projects"));
+            var item4 = new ItemMenu("Projects", menuProject, PackIconKind.FileReport);
+
+            var menuReport = new List<SubItem>();
+            menuReport.Add(new SubItem("View all reports"));
+            var item5 = new ItemMenu("Reports", menuReport, PackIconKind.FileReport);
+
+            var item0 = new ItemMenu("Dashboard", new UserControl(), PackIconKind.ViewDashboard);
+
+            Menu.Children.Add(new UserControlMenuItem(item0, this));
+            Menu.Children.Add(new UserControlMenuItem(item1, this));
+            Menu.Children.Add(new UserControlMenuItem(item2, this));
+            Menu.Children.Add(new UserControlMenuItem(item3, this));
+            Menu.Children.Add(new UserControlMenuItem(item4, this));
+            Menu.Children.Add(new UserControlMenuItem(item5, this));
+        }
+
+        public void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+            if (screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+
+                if (screen.Name == "Administrators")
+                {
+                    AdministratorsView administratorsView = new AdministratorsView();
+                }                
+            }
         }
     }
 }
---
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/Views/UserControlMenuItem.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Zadatak_1.ViewModels;
+
+namespace Zadatak_1.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControlMenuItem.xaml
+    /// </summary>
+    public partial class UserControlMenuItem : UserControl
+    {
+        MasterView _context;
+        AdministratorView adminContext;
+        private bool mouseClicked;
+
+        public UserControlMenuItem(ItemMenu itemMenu, MasterView context)
+        {
+            InitializeComponent();
+            _context = context;
+
+            ExpanderMenu.Visibility = itemMenu.SubItems == null ? Visibility.Collapsed : Visibility.Visible;
+            ListViewItemMenu.Visibility = itemMenu.SubItems == null ? Visibility.Visible : Visibility.Collapsed;
+
+            this.DataContext = itemMenu;
+        }        
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (mouseClicked)
+            {
+                if (e.AddedItems.Count > 0)
+                    _context.SwitchScreen(((SubItem)((ListView)sender).SelectedItem).Screen);
+            }
+
+        }
+        private void ListViewMenu_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mouseClicked = true;
+
+            if (ListViewMenu.SelectedItem != null)
+            {
+                _context.SwitchScreen(((SubItem)((ListView)sender).SelectedItem).Screen);
+            }
+        }
+    }
+}
