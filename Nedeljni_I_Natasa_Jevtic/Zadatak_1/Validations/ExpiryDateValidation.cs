9d60e6a18e38ec148e0036f5ee292070053d76d9
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-08 14:05:11 +0200

    Classes for validation created

---
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/Validations/ExpiryDateValidation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace Zadatak_1.Validations
+{
+    class ExpiryDateValidation : ValidationRule
+    {
+        /// <summary>
+        /// This method checks if expiry date is valid and in specific format.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns>ValidationRule.</returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string expiryDate = value as string;
+            if (DateTime.TryParseExact(expiryDate, "MM/dd/yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime date))
+            {
+                if (date < DateTime.Today)
+                {
+                    return new ValidationResult(false, "Date cannot be in the past.");
+                }
+                else
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+            else
+            {
+                return new ValidationResult(false, "Invalid date. Format: MM/dd/yyyy");
+            }
+        }
+    }
+}
---
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/Validations/UniquePosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using Zadatak_1.Models;
+
+namespace Zadatak_1.Validations
+{
+    class UniquePosition : ValidationRule
+    {
+        /// <summary>
+        /// This methos checks if position is unique.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string positionName = value as string;
+            Positions positions = new Positions();
+            List<vwPosition> positionList = positions.GetAllPositions();
+            var list = positionList.Where(x => x.PositionName == positionName).ToList();
+            //if exists position with forwarded name, return false
+            if (list.Count() > 0)
+            {
+                return new ValidationResult(false, "This position already exists.");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
