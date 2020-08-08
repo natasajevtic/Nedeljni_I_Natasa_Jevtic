5ba6f4e470bec30bf8ac1a00f353c562f3d29e13
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-08 17:14:53 +0200

    Possibility to edit administrator added

---
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/Validations/ValidationForEdit.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Zadatak_1.Models;
+
+namespace Zadatak_1.Validations
+{
+    class ValidationForEdit
+    {
+        public bool JmbgValidation(string jmbg, string oldJmbg)
+        {
+            if (jmbg.Length != 13 || !jmbg.All(Char.IsDigit))
+            {
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    string day = jmbg.Substring(0, 2);
+                    string month = jmbg.Substring(2, 2);
+                    string year = jmbg.Substring(4, 3);
+                    string validyear = "";
+                    if (year[0] == '0')
+                    {
+                        validyear = "2" + year;
+                    }
+                    else
+                    {
+                        validyear = "1" + year;
+                    }
+                    bool conversionYear = Int32.TryParse(year, out int y);
+                    bool conversionMonth = Int32.TryParse(month, out int m);
+                    bool conversionDay = Int32.TryParse(day, out int d);
+                    //checks if passed jmbg contains a valid date
+                    var expectedDatetime = new DateTime(y, m, d);
+                    //gets a birth date from passed jmbg
+                    DateTime dateOfBirth = DateTime.Parse(validyear + "-" + month + "-" + day);
+                    //calculates age 
+                    int ageInDays = (DateTime.Today - dateOfBirth).Days;
+                    //if age less than 15 returns false, else returns true
+                    if (ageInDays < 5479)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (jmbg != oldJmbg)
+                        {
+                            Users users = new Users();
+                            List<tblUser> userList = users.GetAllUsers();
+                            var list = userList.Where(x => x.JMBG == jmbg).ToList();
+                            //if exists employee with forwarded jmbg, return false
+                            if (list.Count() > 0)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+                //if cannot convert to DateTime, because jmbg doesn't contain a valid date
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool UniqueUsername(string username, string oldUsername)
+        {
+            Users users = new Users();
+            List<tblUser> userList = users.GetAllUsers();
+            if (username != oldUsername)
+            {
+                var list = userList.Where(x => x.Username == username).ToList();
+                //if exists employee with forwarded username, return false
+                if (list.Count() > 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
--- Nedeljni_I_Natasa_Jevtic/Zadatak_1/ViewModels/AdministratorEditFormViewModel.cs
+++ Nedeljni_I_Natasa_Jevtic/Zadatak_1/ViewModels/AdministratorEditFormViewModel.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Commands;
 using Zadatak_1.Models;
 using Zadatak_1.Views;
+using Zadatak_1.Validations;
 
 namespace Zadatak_1.ViewModels
 {
     class AdministratorEditFormViewModel : BaseViewModel
     {
-        AdministratorEditFormView administratorFormView;        
+        AdministratorEditFormView administratorFormView;
+        Genders genders = new Genders();
+        MarriageStatus marriageStatus = new MarriageStatus();
+        Administrators administrators = new Administrators();
+        ValidationForEdit validation = new ValidationForEdit();
+
+        public vwAdministrator OldAdministrator { get; set; }
 
         private vwAdministrator administrator;
 
@@ -26,10 +33,171 @@ namespace Zadatak_1.ViewModels
                 OnPropertyChanged("Administrator");
             }
         }
+
+        private List<string> genderList;
+
+        public List<string> GenderList
+        {
+            get
+            {
+                return genderList;
+            }
+            set
+            {
+                genderList = value;
+                OnPropertyChanged("GenderList");
+            }
+        }
+
+        private List<string> marriageStatusList;
+
+        public List<string> MarriageStatusList
+        {
+            get
+            {
+                return marriageStatusList;
+            }
+            set
+            {
+                marriageStatusList = value;
+                OnPropertyChanged("MarriageStatusList");
+            }
+        }
+
+        private List<string> administratorTypeList;
+
+        public List<string> AdministratorTypeList
+        {
+            get
+            {
+                return administratorTypeList;
+            }
+            set
+            {
+                administratorTypeList = value;
+                OnPropertyChanged("AdministratorTypeList");
+            }
+        }
+
+        private ICommand saveAdministrator;
+        public ICommand SaveAdministrator
+        {
+            get
+            {
+                if (saveAdministrator == null)
+                {
+                    saveAdministrator = new RelayCommand(param => SaveAdministratorExecute(), param => CanSaveAdministratorExecute());
+                }
+                return saveAdministrator;
+            }
+        }
+
+        private ICommand cancelAdministrator;
+        public ICommand CancelAdministrator
+        {
+            get
+            {
+                if (cancelAdministrator == null)
+                {
+                    cancelAdministrator = new RelayCommand(param => CancelAdministratorExecute(), param => CanCancelAdministratorExecute());
+                }
+                return cancelAdministrator;
+            }
+        }
+
         public AdministratorEditFormViewModel(AdministratorEditFormView administratorFormView, vwAdministrator administratorToEdit)
         {
             this.administratorFormView = administratorFormView;
-            Administrator = administratorToEdit;            
+            Administrator = administratorToEdit;
+            GenderList = genders.GetGenders();
+            MarriageStatusList = marriageStatus.GetMarriageStatus();
+            AdministratorTypeList = administrators.GetAdministratorType();
+            //gets administrator initial values before editing
+            OldAdministrator = new vwAdministrator
+            {
+                AccountExpirationDate = administratorToEdit.AccountExpirationDate,
+                Gender = administratorToEdit.Gender,
+                MarriageStatus = administratorToEdit.MarriageStatus,
+                Residence = administrator.Residence,
+                TypeOfAdministrator = administratorToEdit.TypeOfAdministrator,
+                JMBG = administratorToEdit.JMBG,
+                Name = administratorToEdit.Name,
+                Password = administratorToEdit.Password,
+                Surname = administratorToEdit.Surname,
+                Username = administrator.Username
+            };
+        }
+
+        public void SaveAdministratorExecute()
+        {
+            try
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to save the administrator?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    bool isCreated = administrators.EditAdministrator(Administrator);
+                    if (isCreated == true)
+                    {
+                        MessageBox.Show("Administrator is edited.", "Notification", MessageBoxButton.OK);
+                        administratorFormView.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Administrator cannot be edited.", "Notification", MessageBoxButton.OK);
+                        administratorFormView.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public bool CanSaveAdministratorExecute()
+        {
+            if ((Administrator.Name != OldAdministrator.Name || Administrator.Surname != OldAdministrator.Surname || Administrator.Gender != OldAdministrator.Gender ||
+                Administrator.Residence != OldAdministrator.Residence || Administrator.MarriageStatus != OldAdministrator.MarriageStatus ||
+                Administrator.AccountExpirationDate != OldAdministrator.AccountExpirationDate || Administrator.TypeOfAdministrator != OldAdministrator.TypeOfAdministrator
+                      || Administrator.Username != OldAdministrator.Username || Administrator.Password != OldAdministrator.Password)
+                      &&
+                      (!String.IsNullOrEmpty(Administrator.Name) && !String.IsNullOrEmpty(Administrator.Surname) && !String.IsNullOrEmpty(Administrator.JMBG) && !String.IsNullOrEmpty(Administrator.Gender)
+               && !String.IsNullOrEmpty(Administrator.Residence) && !String.IsNullOrEmpty(Administrator.MarriageStatus) && !String.IsNullOrEmpty(Administrator.Username)
+               && !String.IsNullOrEmpty(Administrator.Password) && !String.IsNullOrEmpty(Administrator.AccountExpirationDate.ToString()) && !String.IsNullOrEmpty(Administrator.TypeOfAdministrator)))
+            {
+                if (validation.JmbgValidation(Administrator.JMBG, OldAdministrator.JMBG) && validation.UniqueUsername(Administrator.JMBG, OldAdministrator.JMBG))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CancelAdministratorExecute()
+        {
+            try
+            {
+                MessageBoxResult result = MessageBox.Show("Are you sure you want to cancel creating the administrator?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    administratorFormView.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public bool CanCancelAdministratorExecute()
+        {
+            return true;
         }
     }
 }
